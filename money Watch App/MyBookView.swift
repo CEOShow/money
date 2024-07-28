a179d88b0e4cdafd269c39a334c0d0b7e1924620
CEOShow <showhihorse@gmail.com>
2024-07-28 19:17:54 +0800

    將 ExpenseDetailView 與 MyBookView 分開

--- money Watch App/MyBookView.swift
+++ money Watch App/MyBookView.swift
@@ -85,50 +85,6 @@ struct MyBookView: View {
     }
 }
 
-struct ExpenseDetailView: View {
-    let expenses: [Expense]
-    
-    var body: some View {
-        List(expenses) { expense in
-            ExpenseRow(expense: expense)
-        }
-        .navigationTitle("詳細明細")
-    }
-}
-
-struct ExpenseRow: View {
-    let expense: Expense
-    
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text(expense.note)
-                    .font(.headline)
-                Text(formatDate(expense.date))
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
-            Spacer()
-            Text(formatCurrency(expense.income))
-                .font(.headline)
-                .foregroundColor(expense.income >= 0 ? .green : .red)
-        }
-    }
-    
-    private func formatDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .short
-        return formatter.string(from: date)
-    }
-    
-    private func formatCurrency(_ amount: Double) -> String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.currencyCode = "TWD"  // 您可能想要根據帳本的幣別來設定這個
-        return formatter.string(from: NSNumber(value: amount)) ?? "\(amount)"
-    }
-}
 
 struct MyBookView_Previews: PreviewProvider {
     static var previews: some View {
